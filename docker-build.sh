5592b1d830b3132fafca767f68be6ebbb484903a
Pengcheng Wang <pewang@kth.se>
2024-05-21 15:15:27 +0200

    update to v2.22.0

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 #Build and push docker image
-RAY_VERSION=2.12.0
+RAY_VERSION=2.22.0
 docker build --build-arg BASE_IMAGE=$RAY_VERSION --progress=plain --tag registry.service.consul:4443/ray-ml:$RAY_VERSION .
 docker push registry.service.consul:4443/ray-ml:$RAY_VERSION
